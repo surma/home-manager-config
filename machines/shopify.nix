9dfd94ac0753cb4cee5e0d1f8359d046c9a367ca
Surma <surma@surma.dev>
2024-07-16 18:12:23 +0100

    More shopify config

--- machines/shopify.nix
+++ machines/shopify.nix
@@ -4,10 +4,15 @@ lib.recursiveUpdate prev {
   home.username = "surma";
   home.homeDirectory = "/Users/surma";
 
-  programs.git.extraConfig.core = {
-    untrackedCache = true;
-    fsmonitor = true;
-    fsmonitorhookversion = 2;
+  programs.git.extraConfig = {
+    core = {
+      untrackedCache = true;
+      fsmonitor = true;
+      fsmonitorhookversion = 2;
+    };
+    include = {
+      path = "${final.home.homeDirectory}/.config/dev/gitconfig";
+    };
   };
 
   programs.zsh = {
@@ -15,6 +20,8 @@ lib.recursiveUpdate prev {
       prev.programs.zsh.initExtra
       + ''
         [ -f /opt/dev/dev.sh ] && source /opt/dev/dev.sh
+        [[ -f /opt/dev/sh/chruby/chruby.sh ]] && { type chruby >/dev/null 2>&1 || chruby () { source /opt/dev/sh/chruby/chruby.sh; chruby "$@"; }
+        [[ -x /opt/homebrew/bin/brew ]] && eval $(/opt/homebrew/bin/brew shellenv)
       '';
     shellAliases = {
       hms = "home-manager switch -A shopify";
