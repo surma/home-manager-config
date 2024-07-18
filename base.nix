4bf7ee322ac757be92d59da5620f53ac360374f8
Surma <surma@surma.dev>
2024-07-18 09:08:36 +0100

    Custom scripts

--- base.nix
+++ base.nix
@@ -29,11 +29,14 @@ final: {
       fira-code
       git
       gitui
+      git-lfs
       htop
       jq
       nil
       nixfmt-rfc-style
+      nodejs_22
       obsidian
+      ollama
       openssh
       raycast
       rsync
@@ -56,33 +59,14 @@ final: {
       fenix.targets.wasm32-unknown-unknown.stable.rust-std
       fenix.targets.wasm32-wasi.stable.rust-std
       rust-analyzer
-    ];
+    ]
+    ++ [ (import ./scripts { inherit pkgs; }) ];
 
   home.file = {
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+    ".npmrc".source = ./configs/npmrc;
+    # ".gradle/gradle.properties".text = "bla";
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/surma/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
     EDITOR = "hx";
   };
---
+++ scripts/default.nix
@@ -0,0 +1,16 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+with pkgs;
+stdenv.mkDerivation {
+  name = "scripts";
+  src = lib.cleanSourceWith {
+    filter = path: _type: !lib.strings.hasSuffix ".nix" path;
+    src = lib.cleanSource ./.;
+  };
+  phases = [ "installPhase" ];
+  installPhase = ''
+    mkdir -p $out/bin
+    cp -r $src/* $out/bin
+  '';
+}
