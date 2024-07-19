b9a33445ab5986d9ba5d2876610ae03d86d48801
Surma <surma@surma.dev>
2024-07-19 12:28:28 +0100

    Fix shopify zshrc additions

--- machines/shopify.nix
+++ machines/shopify.nix
@@ -20,8 +20,8 @@ lib.recursiveUpdate prev {
       prev.programs.zsh.initExtra
       + ''
         [ -f /opt/dev/dev.sh ] && source /opt/dev/dev.sh
-        [[ -f /opt/dev/sh/chruby/chruby.sh ]] && { type chruby >/dev/null 2>&1 || chruby () { source /opt/dev/sh/chruby/chruby.sh; chruby "$@"; }
-        [[ -x /opt/homebrew/bin/brew ]] && eval $(/opt/homebrew/bin/brew shellenv)
+        [ -f /opt/dev/sh/chruby/chruby.sh ] && { type chruby >/dev/null 2>&1 || chruby () { source /opt/dev/sh/chruby/chruby.sh; chruby "$@"; } }
+        [ -x /opt/homebrew/bin/brew ] && eval $(/opt/homebrew/bin/brew shellenv)
       '';
     shellAliases = {
       hms = "home-manager switch -A shopify";
