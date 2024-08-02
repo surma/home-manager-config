050f5d41ec37bd5a93de749517a8ba10c5c37529
Surma <surma@surma.dev>
2024-08-02 18:21:10 +0100

    Automatically loads machines folders

--- home.nix
+++ home.nix
@@ -1,7 +1,21 @@
-rec {
-  default = surmbook;
-  surmbook = ./machines/surmbook.nix;
-  shopify = ./machines/shopify.nix;
+let
+  pkgs = import <nixpkgs> { };
+  lib = pkgs.lib;
+  machinesDir = "${./.}/machines";
+  machinesDirContent = builtins.readDir machinesDir;
+  machineFiles = lib.filter (file: lib.strings.hasSuffix ".nix" file) (
+    lib.attrNames machinesDirContent
+  );
+  machines = lib.listToAttrs (
+    lib.map (file: {
+      name = lib.strings.removeSuffix ".nix" file;
+      value = "${machinesDir}/${file}";
+    }) machineFiles
+  );
+in
+machines
+// rec {
+  default = machines.surmbook;
 
   # This is what makes `home-manager switch` enable the `default` config
   # when no explicit `-A <profile name>` is present.
