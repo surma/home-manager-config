ab2081a2e260eb3e07fa900f959754035e153727
Surma <surma@surma.dev>
2024-08-18 00:30:27 +0100

    Builds on mac

--- extra-pkgs/greenlight/default.nix
+++ extra-pkgs/greenlight/default.nix
@@ -1,6 +1,8 @@
 {
   lib,
   fetchFromGitHub,
+  jq,
+  yq,
   nodejs,
   fetchYarnDeps,
   mkYarnPackage,
@@ -15,21 +17,54 @@
 let
   version = "2.3.1";
 
-  downloadElectron =
-    {
-      version,
-      arch,
-      os,
-      hash,
-    }:
+  srcHash = "sha256-3csPU6Qbvrj6nUZFmxg+EIrCs3Z5LwyLceycQp2FXXo=";
+  electronHash = "sha256:1k4dzcmp596fzvqr82drzv6cymarihxxqk706sgfm9a77hrrn493";
+  depHash = "sha256-KW98Itndh437Uni9/HtTCSeSF4JqV7lQucDPZbtMyxQ=";
+
+  systemObj = lib.systems.parse.mkSystemFromString system;
+  cpu = systemObj.cpu.name;
+  arch = if (cpu == "aarch64") then "arm64" else cpu;
+
+  kernel = systemObj.kernel.name;
+  os = if (kernel == "darwin") then "mac" else kernel;
+
+  src = fetchFromGitHub {
+    owner = "unknownskl";
+    repo = "greenlight";
+    rev = "v${version}";
+    fetchSubmodules = true;
+    hash = srcHash;
+  };
+
+  yarnlock-to-json = callPackage (import ./yarnlock-to-json) { };
+  electronVersion = lib.readFile (
+    stdenv.mkDerivation {
+      name = "electron-version";
+      inherit src;
+      nativeBuildInputs = [
+        yarnlock-to-json
+        jq
+      ];
+      # dontUnpack = true;
+      dontInstall = true;
+      # phases = ["buildPhase"];
+      buildPhase = ''
+        cat yarn.lock | \
+        yarnlock-to-json  | \
+        jq -j 'to_entries | map(select(.key | startswith("electron@"))) | .[0].value.version' > $out
+      '';
+    }
+  );
+
+  electronCache =
     let
-      name = "electron-v${version}-${os}-${arch}";
+      name = "electron-v${electronVersion}-${kernel}-${arch}";
     in
     stdenv.mkDerivation {
-      inherit name;
+      name = "electron-zip";
       tarball = builtins.fetchurl {
-        url = "https://github.com/electron/electron/releases/download/v${version}/${name}.zip";
-        sha256 = hash;
+        url = "https://github.com/electron/electron/releases/download/v${electronVersion}/${name}.zip";
+        sha256 = electronHash;
       };
       dontUnpack = true;
       phases = [ "installPhase" ];
@@ -39,35 +74,9 @@ let
       '';
     };
 
-  src = fetchFromGitHub {
-    owner = "unknownskl";
-    repo = "greenlight";
-    rev = "v${version}";
-    fetchSubmodules = true;
-    hash = "sha256-3csPU6Qbvrj6nUZFmxg+EIrCs3Z5LwyLceycQp2FXXo=";
-  };
-
-  electronCache = downloadElectron {
-    version = "28.2.2";
-    arch = "arm64";
-    os = "darwin";
-    hash = "sha256:1k4dzcmp596fzvqr82drzv6cymarihxxqk706sgfm9a77hrrn493";
-  };
-
   sips = writeShellScriptBin "sips" ''
     exec /usr/bin/sips "$@"
   '';
-
-  patchElectronBuilderConfig = callPackage (import ./patch-electron-builder-config) {
-    inherit nodejs;
-  };
-
-  systemObj = lib.systems.parse.mkSystemFromString system;
-  cpu = systemObj.cpu.name;
-  arch = if (cpu == "aarch64") then "arm64" else cpu;
-
-  kernel = systemObj.kernel.name;
-  os = if (kernel == "darwin") then "mac" else kernel;
 in
 mkYarnPackage {
   pname = "greenlight";
@@ -78,7 +87,7 @@ mkYarnPackage {
   packageJSON = "${src}/package.json";
   offlineCache = fetchYarnDeps {
     yarnLock = "${src}/yarn.lock";
-    hash = "sha256-KW98Itndh437Uni9/HtTCSeSF4JqV7lQucDPZbtMyxQ=";
+    hash = depHash;
   };
 
   nativeBuildInputs = [
@@ -86,17 +95,17 @@ mkYarnPackage {
     which
     python3
     undmg
-    patchElectronBuilderConfig
+    yq
   ];
 
   patchPhase = ''
-    patch-electron-builder-config $PWD/electron-builder.yml $PWD/yarn.lock ${kernel} ${arch} $TMPDIR/cache
+    mv electron-builder.yml{,.old}
+    cat electron-builder.yml.old | \
+    yq -y '. + {"electronDownload": {"arch": "${arch}", "platform": "${kernel}", "version": "${electronVersion}", "cache": "'$electronCache'"}} | .mac.target = [{"target": "dir", "arch": "${arch}"}] | .linux.target = [{"target": "dir", "arch": "${arch}"}]' > electron-builder.yml
   '';
 
   configurePhase = ''
     ln -s $node_modules node_modules
-    mkdir -p $TMPDIR/cache
-    cp $electronCache/* $TMPDIR/cache/
   '';
 
   buildPhase = ''
