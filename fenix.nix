cbb4d3738398f7eadd47f4f07ce82411da1974d1
Surma <surma@surma.dev>
2024-08-29 13:54:40 +0100

    Update fenix

--- fenix.nix
+++ fenix.nix
@@ -2,12 +2,6 @@
   pkgs ? import <nixpkgs> { },
 }:
 fetchTarball {
-  url = "https://github.com/nix-community/fenix/archive/1270fb024c6987dd825a20cd27319384a8d8569e.tar.gz";
-  sha256 = "sha256-GKlvM9M0mkKJrL6N1eMG4DrROO25Ds1apFw3/b8594w=";
+  url = "https://github.com/nix-community/fenix/archive/2d808ed09caffa0984a6d54bc1558959d48b012b.tar.gz";
+  sha256 = "sha256:1cm2qa2w5rp3y90rwryqy0iqlm3j9dx8wqva0cdhjlqk2ykhc00a";
 }
-# pkgs.fetchFromGitHub { 
-# 	owner = "nix-community";
-# 	repo = "fenix";
-# 	rev = "1270fb024c6987dd825a20cd27319384a8d8569e";
-# 	hash = "sha256-GKlvM9M0mkKJrL6N1eMG4DrROO25Ds1apFw3/b8594w=";
-# }
