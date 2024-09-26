cccca6018ba335a700f340fb8a3c6316ba93f8ac
Surma <surma@surma.dev>
2024-09-26 22:57:52 +0000

    Remove close keybind in zellij

--- modules/base.nix
+++ modules/base.nix
@@ -96,30 +96,7 @@ in
       enable = true;
     };
     programs.zsh = import ../configs/zsh.nix;
-    programs.zellij = {
-      enable = true;
-      settings = {
-        pane_frames = false;
-        session_serialization = false;
-
-        theme = "gruvbox";
-        themes = {
-          gruvbox = {
-            fg = "#D5C4A1";
-            bg = "#282828";
-            black = "#3C3836";
-            red = "#CC241D";
-            green = "#98971A";
-            yellow = "#D79921";
-            blue = "#3C8588";
-            magenta = "#B16286";
-            cyan = "#689D6A";
-            white = "#FBF1C7";
-            orange = "#D65D0E";
-          };
-        };
-      };
-    };
+    programs.zellij = import ../configs/zellij.nix;
     programs.ssh = {
       enable = true;
       forwardAgent = true;
