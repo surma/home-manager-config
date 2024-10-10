b8be01e05c6f601bc400fbe198ddee2029ab4107
Surma <surma@surma.dev>
2024-10-11 00:00:53 +0100

    Add some system config

--- darwin/base.nix
+++ darwin/base.nix
@@ -1,4 +1,8 @@
-{ ... }@args:
+{ pkgs, system, ... }@args:
+let
+  x = 1;
+in
+# pkgs = nixpkgs.legacyPackages.${system};
 
 {
   system.stateVersion = 5;
@@ -9,5 +13,22 @@
 
   services.nix-daemon.enable = true;
 
+  fonts.packages = with pkgs; [ fira-code ];
+
+  homebrew = {
+    enable = true;
+  };
+
+  # networking.computerName = "";
+  # networking.hostName = "";
   # system.configurationRevision = self.rev or self.dirtyRev or null;
+
+  system.defaults.NSGlobalDomain = {
+    InitialKeyRepeat = 2;
+    KeyRepeat = 15;
+    NSAutomaticCapitalizationEnabled = false;
+    NSAutomaticDashSubstitutionEnabled = false;
+    NSAutomaticPeriodSubstitutionEnabled = false;
+    NSAutomaticQuoteSubstitutionEnabled = false;
+  };
 }
