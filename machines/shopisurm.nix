e34c90a61284f92700a9f62739bb0852a9dadc62
Surma <surma@surma.dev>
2024-10-21 14:45:35 +0100

    Use correct code snippets for dev

--- machines/shopisurm.nix
+++ machines/shopisurm.nix
@@ -17,6 +17,7 @@ in
         podman
         podman-compose
         graphite-cli
+        python3
       ])
       ++ [
         (callPackage (import ../extra-pkgs/vfkit) { })
@@ -39,8 +40,8 @@ in
     programs.zsh = {
       initExtra = ''
         [ -f /opt/dev/dev.sh ] && source /opt/dev/dev.sh
-        [ -f /opt/dev/sh/chruby/chruby.sh ] && { type chruby >/dev/null 2>&1 || chruby () { source /opt/dev/sh/chruby/chruby.sh; chruby "$@"; } }
-        [ -x /opt/homebrew/bin/brew ] && eval $(/opt/homebrew/bin/brew shellenv)
+        [[ -f /opt/dev/sh/chruby/chruby.sh ]] && { type chruby >/dev/null 2>&1 || chruby () { source /opt/dev/sh/chruby/chruby.sh; chruby "$@"; } }
+        [[ -x /opt/homebrew/bin/brew ]] && eval $(/opt/homebrew/bin/brew shellenv)
       '';
     };
     programs.ssh = {
