8df34be0b8db89275c9b564d1fbd3054ba133265
Surma <surma@surma.dev>
2024-10-21 14:43:40 +0100

    Use nix-darwin for hms alias

--- modules/macos.nix
+++ modules/macos.nix
@@ -24,9 +24,17 @@ in
 
     home.file.".config/aerospace/aerospace.toml".source = ../configs/aerospace.toml;
 
+    home.sessionVariables.CONFIG_MANAGER = "darwin-rebuild";
+
     # Use 1password to unlock SSH key
     programs.ssh.matchBlocks."*".extraOptions = {
       "IdentityAgent" = ''"${config.home.homeDirectory}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"'';
+    };
+
+    programs.zsh = {
+      initExtra = ''
+        export PATH=$PATH:/run/current-system/sw/bin
+      '';
     };
   };
 }
