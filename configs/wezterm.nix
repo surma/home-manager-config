2b3a7fd4b0cb2d0bc9959fb68ed689bf39028d78
Surma <surma@surma.dev>
2025-01-22 09:50:06 +0000

    Switch wezterm to webgpu

--- configs/wezterm.nix
+++ configs/wezterm.nix
@@ -10,11 +10,10 @@
       left = "1cell",
       right = "1cell",
     }
+    config.front_end = "WebGpu"
     config.color_scheme = 'Gruvbox Dark (Gogh)'
     config.font = wezterm.font 'Fira Code'
     config.font_size = 14.0
-    config.window_background_opacity = 0.9
-    config.macos_window_background_blur = 30
     config.window_decorations = 'RESIZE'
     config.window_frame = {
       font = wezterm.font({ family = 'Fira Code', weight = 'Bold' }),
