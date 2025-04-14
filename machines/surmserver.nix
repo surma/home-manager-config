86033251a7e36a4b46dca7b2678f1b3cdd7a7364
Surma <surma@surma.dev>
2025-04-14 14:43:13 +0200

    Update update script

--- machines/surmserver.nix
+++ machines/surmserver.nix
@@ -2,7 +2,13 @@
 {
   config = {
 
-    home.packages = (with pkgs; [ google-cloud-sdk sqlite ]);
+    home.packages = (
+      with pkgs;
+      [
+        google-cloud-sdk
+        sqlite
+      ]
+    );
 
     home.sessionVariables.FLAKE_CONFIG_URI = "path:${config.home.homeDirectory}/.config/home-manager#surmserver";
   };
