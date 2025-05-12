b2c8e3ad0217b41122a000188b919d42a629dcae
Surma <surma@surma.dev>
2025-05-12 10:38:57 +0100

    Auto-backup on darwin

--- load-darwin.nix
+++ load-darwin.nix
@@ -10,9 +10,6 @@
   darwinModules,
 }:
 let
-  pkgs = nixpkgs.legacyPackages.${system};
-  inherit (pkgs) lib;
-
   loadConfig = import ./load-config.nix args;
 
   hmConfig = loadConfig system hmModules;
@@ -24,7 +21,7 @@ in
 nix-darwin.lib.darwinSystem {
   system = system;
   modules = darwinModules ++ [
-    args.home-manager.darwinModules.home-manager
+    home-manager.darwinModules.home-manager
     {
       users.users.surma = {
         name = userData.username;
@@ -33,6 +30,7 @@ nix-darwin.lib.darwinSystem {
 
       home-manager = {
         users.${userData.username}.imports = hmModules;
+        backupFileExtension = "bak";
         extraSpecialArgs = args;
       };
     }
