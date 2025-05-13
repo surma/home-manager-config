faebdbaa965cef7590da58b858f4c160a0eccd6a
Surma <surma@surma.dev>
2025-05-13 18:58:02 +0100

    Turn ghclone into a nu script

--- scripts/default.nix
+++ scripts/default.nix
@@ -1,14 +1,22 @@
-{ stdenv, lib }:
+{
+  stdenv,
+  lib,
+  pkgs,
+}:
+let
+  inherit (pkgs) nushell;
+in
 stdenv.mkDerivation {
   name = "scripts";
+  buildInputs = [ nushell ];
   src = lib.cleanSourceWith {
     filter = path: _type: !lib.strings.hasSuffix ".nix" path;
     src = lib.cleanSource ./.;
   };
   installPhase = ''
     mkdir -p $out/bin
-    patchShebangs $src
     cp -r $src/* $out/bin
+    patchShebangs $out/bin/*
     chmod +x $out/bin/*
   '';
 }
