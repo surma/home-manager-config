26bd1ef1b8351f8c0f8797e2ac28e4e13be0fdd2
Surma <surma@surma.dev>
2025-06-24 00:13:39 +0100

    wezterm fixes

--- configs/wezterm.nix
+++ configs/wezterm.nix
@@ -4,15 +4,10 @@
 }:
 let
   package = callPackage (import ../extra-pkgs/wezterm) { };
-
-  wrapper = writeShellScriptBin "wezterm" ''
-    ${package}/bin/wezterm --config 'front_end="OpenGL"'
-  '';
 in
 {
   wezterm = {
-    # inherit package;
-    package = wrapper;
+    inherit package;
 
     enable = true;
     extraConfig = ''
@@ -39,19 +34,19 @@ in
       end)
 
       local config = wezterm.config_builder()
+      -- config.front_end = "WebGpu"
       config.front_end = "OpenGL"
       config.send_composed_key_when_left_alt_is_pressed = true
       config.enable_tab_bar = false
       config.window_padding = {
         top = "0.5cell",
-        bottom = "0.5cell",
+        bottom = "1.5cell",
         left = "1cell",
         right = "1cell",
       }
-      config.front_end = "WebGpu"
       config.color_scheme = 'Gruvbox Dark (Gogh)'
       config.font = wezterm.font 'Fira Code'
-      config.font_size = 14.0
+      config.font_size = 12.0
       config.window_decorations = 'RESIZE'
       config.window_frame = {
         font = wezterm.font({ family = 'Fira Code', weight = 'Bold' }),
