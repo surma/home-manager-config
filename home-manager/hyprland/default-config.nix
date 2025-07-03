e75356cc6ec3e60fb80aacaee298461e45eb9b15
Surma <surma@surma.dev>
2025-07-04 00:06:42 +0100

    Done the rest

--- home-manager/hyprland/default-config.nix
+++ home-manager/hyprland/default-config.nix
@@ -130,17 +130,50 @@ with lib;
           flags.e = true;
           flags.l = true;
         }
-        # bindel = ,XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+
-        # bindel = ,XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
-        # bindel = ,XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
-        # bindel = ,XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
-        # bindel = ,XF86MonBrightnessUp, exec, brightnessctl -e4 -n2 set 5%+
-        # bindel = ,XF86MonBrightnessDown, exec, brightnessctl -e4 -n2 set 5%-
-
-        # bindl = , XF86AudioNext, exec, playerctl next
-        # bindl = , XF86AudioPause, exec, playerctl play-pause
-        # bindl = , XF86AudioPlay, exec, playerctl play-pause
-        # bindl = , XF86AudioPrev, exec, playerctl previous
+        {
+          key = ",XF86AudioMute";
+          action.exec = "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
+          flags.e = true;
+          flags.l = true;
+        }
+        {
+          key = ",XF86AudioMicMute";
+          action.exec = "wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle";
+          flags.e = true;
+          flags.l = true;
+        }
+        {
+          key = ",XF86MonBrightnessUp";
+          action.exec = "brightnessctl -e4 -n2 set 5%+";
+          flags.e = true;
+          flags.l = true;
+        }
+        {
+          key = ",XF86MonBrightnessDown";
+          action.exec = "brightnessctl -e4 -n2 set 5%-";
+          flags.e = true;
+          flags.l = true;
+        }
+        {
+          key = ",XF86AudioNext";
+          action.exec = "playerctl next";
+          flags.l = true;
+        }
+        {
+          key = ",XF86AudioPause";
+          action.exec = "playerctl play-pause";
+          flags.l = true;
+        }
+        {
+          key = ",XF86AudioPlay";
+          action.exec = "playerctl play-pause";
+          flags.l = true;
+        }
+        {
+          key = ",XF86AudioPrev";
+          action.exec = "playerctl previous";
+          flags.l = true;
+        }
       ];
     };
   };
