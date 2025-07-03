c9b0294298b265333b1fc9f60d76f0a3ad5c6ffb
Surma <surma@surma.dev>
2025-07-03 14:34:48 +0100

    Downgrade v4l2loopback

--- machines/surmframework.nix
+++ machines/surmframework.nix
@@ -5,6 +5,9 @@
   lib,
   ...
 }:
+let
+  v4l2loopback-0132 = pkgs.callPackage ../extra-pkgs/v4l2loopback-0132 { inherit config; };
+in
 {
   imports = [
     ../home-manager/unfree-apps.nix
@@ -19,13 +22,16 @@
     ../nixos/_1password-wrapper.nix
   ];
 
-  boot.extraModulePackages = with config.boot.kernelPackages; [
-    v4l2loopback
+  boot.extraModulePackages = [
+    v4l2loopback-0132
   ];
 
   boot.kernelModules = [
     "v4l2loopback"
   ];
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+  '';
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
